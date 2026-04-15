7220d1aa58b26e2b42c2446d054e113c321a4969
CarlosMahecha6 <corojrtailandia11@gmail.com>
2026-04-15 17:46:30 -0500

    Archivos finales de docs

--- source/conf.py
+++ source/conf.py
@@ -1,13 +1,4 @@
-import sphinx_rtd_theme
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Semillero Sophia'
 copyright = '2026, CarlosMahecha'
@@ -15,7 +6,6 @@ author = 'CarlosMahecha'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
@@ -24,22 +14,36 @@ exclude_patterns = []
 
 language = 'es'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_baseurl = ""
-html_favicon = "_static/logoSophia.png"
-html_title = "Documentación"
+# -- HTML output -------------------------------------------------------------
+
 html_theme = "sphinx_rtd_theme"
+
+# 🔥 CLAVE: esto arregla GitHub Pages
+html_baseurl = '/semillero-sophia-docs/'
+
+html_title = "Documentación"
+html_favicon = "_static/logoSophia.png"
+html_logo = "_static/logoSophia.png"
+
 html_theme_options = {
     'collapse_navigation': False,
-    'navigation_depth': 2,
+    'navigation_depth': 4,
 }
-# Sirve para ubicar la carpeta que contiene el CSS
-html_static_path = ['_static']  
-# Carga el archivo que contiene el diseño (CSS)
+
+# Archivos estáticos
+html_static_path = ['_static']
+
+# CSS personalizado (opcional)
 html_css_files = [
     'custom.css',
 ]
 
-html_js_files = []          # evita conflictos de rutas JS
-html_extra_path = []        # evita rutas raras
+# ⚠️ IMPORTANTE: NO vaciar esto
+# (Sphinx necesita sus JS por defecto)
+# html_js_files = []
+# html_extra_path = []
+
+# Opcional (mejora compatibilidad)
+html_context = {
+    "display_github": True,
+}
